b4e78fd565be9bb307536499defc2e1dcbba756e
Matthew Heinisch <fgd233@hotmail.com>
2023-10-09 00:53:39 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,118 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+function search(str) {
+  let results = [];
+  const currStr = str.toLowerCase();
+  console.log("search str = " + currStr);
+  results.push(fruit.filter((fruit) => fruit.includes(currStr)));
+  return results;
+}
+
+function searchHandler(e) {
+  // TODO
+  const barInput = e.currentTarget.value;
+  let resultArr = [];
+  if (barInput !== "") {
+    resultArr = search(barInput);
+    console.log("result array = " + resultArr);
+  }
+  showSuggestions(resultArr, barInput);
+}
+
+function showSuggestions(resultArr, inputVal) {
+  suggestions.innerHTML = "";
+  console.log(resultArr);
+  for (let i = 0; i < resultArr.length; i++) {
+    console.log(i);
+  }
+}
+
+function useSuggestion(e) {
+  // TODO
+}
+
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
